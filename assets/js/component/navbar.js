204a3066c382a986b0250ac56a384ca3073e0263
Iqbal <iqbalnugraha347@gmail.com>
2021-05-22 00:43:22 +0700

    add new result scrap data

--- assets/js/component/navbar.js
+++ assets/js/component/navbar.js
@@ -1,16 +1,16 @@
 const myNavbar = () => {
     let navbarHTML = `
     <div class="logo__container">
-        <h1><a href="http://localhost/ngobatin/admin" target="_blank"><img class="logo" src="./assets/img/logo.png" alt="logo"> Ngobatin<span>.</span></a></h1>
+        <h1><a href="http://localhost/ngobatin/admin/user" target="_blank"><img class="logo" src="./assets/img/logo.png" alt="logo"> Ngobatin<span>.</span></a></h1>
     </div>
     <nav>
         <ul class="nav__links">
             <li><a href="index.html">Home</a></li>
             <li><a href="about.html">About</a></li>
-            <li><a href="#">Article</a></li>
+            <li><a href="http://localhost/ngobatin/article/read">Article</a></li>
         </ul>
     </nav>
-    <a class="button button__primary" href=""><i class="fas fa-hand-holding-usd"></i></a>
+    <a class="button button__primary" href="detail.html"><i class="fas fa-hand-holding-usd"></i></a>
     `
     document.getElementById('header').innerHTML = navbarHTML;
 }
